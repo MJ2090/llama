674c5b6f17de66390c049ff8d8490d7e01a0fe07
mj <125602296+MJ2090@users.noreply.github.com>
2023-03-24 11:46:24 -0700

    Update test.py

--- test.py
+++ test.py
@@ -2,13 +2,14 @@ import transformers
 
 tokenizer = transformers.LlamaTokenizer.from_pretrained("/home/zhuminjun01/output1/llama-7B")
 model = transformers.LlamaForCausalLM.from_pretrained("/home/zhuminjun01/output1/llama-7B")
+model.to("cuda")
 
 batch = tokenizer(
-    "The primary use of LLaMA is research on large language models, including",
+    "Write an alticle about solar system ",
     return_tensors="pt", 
     add_special_tokens=False
 )
 batch = {k: v.cuda() for k, v in batch.items()}
-generated = model.generate(batch["input_ids"], max_length=100)
+generated = model.generate(batch["input_ids"], max_length=500)
 
 print(tokenizer.decode(generated[0]))
