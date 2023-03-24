55604e0c689121fd60f647c573ca78d968d7bfbf
mj <125602296+MJ2090@users.noreply.github.com>
2023-03-24 11:48:06 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 import transformers
+print("import transformers done")
 
 tokenizer = transformers.LlamaTokenizer.from_pretrained("/home/zhuminjun01/output1/llama-7B")
 model = transformers.LlamaForCausalLM.from_pretrained("/home/zhuminjun01/output1/llama-7B")
 model.to("cuda")
+print("import tokenizer and model done")
 
 batch = tokenizer(
     "Write an alticle about solar system ",
@@ -10,6 +12,7 @@ batch = tokenizer(
     add_special_tokens=False
 )
 batch = {k: v.cuda() for k, v in batch.items()}
+print("begin generating...")
 generated = model.generate(batch["input_ids"], max_length=500)
 
 print(tokenizer.decode(generated[0]))
