bec16a1dfdb69e455f1613381dfbc267527f72c4
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-03-29 09:39:57 -0700

    s

--- mem/helper.py
+++ mem/helper.py
@@ -12,3 +12,6 @@ def print_summary(result):
     print(f"Time: {result.metrics['train_runtime']:.2f}")
     print(f"Samples/second: {result.metrics['train_samples_per_second']:.2f}")
     print_gpu_utilization()
+
+
+print_gpu_utilization()
